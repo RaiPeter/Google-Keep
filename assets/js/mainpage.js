435ce50d5b4f175693484b924df1cb3bcca44f79
RaiPeter <rai.peter2@gmail.com>
2020-01-17 23:48:06 +0530

    Auto-generated commit

---
+++ assets/js/mainpage.js
@@ -0,0 +1,22 @@
+//   //opening note
+//   document.getElementById('inputNoteMain').addEventListener('click',()=>{
+//     document.getElementById('opennote').style.display="block";
+//     document.getElementById('inputNote').focus();     
+//     document.getElementById('notebarmain').style.display="none";
+
+//   })
+//   document.getElementById('close').addEventListener('click',()=>{
+//     // document.getElementById('icons').style.display="none";
+//     document.getElementById('opennote').style.display="none";
+//     document.getElementById('notebarmain').style.display="block";
+    
+//   })
+//   document.getElementById('upload').addEventListener('click',()=>{
+//     document.getElementById('file').click();
+//   })
+//   document.getElementById('uploadfile').addEventListener('click',()=>{
+// document.getElementById('file').click();
+// document.getElementById('opennote').style.display="block";
+// document.getElementById('inputNote').focus();     
+// document.getElementById('notebarmain').style.display="none";  
+// })
